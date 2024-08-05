f0d2ce0f19564c40300569eb52844c1a6f7f6f85
Dmitry Blokhin <demonblo007@gmail.com>
2024-08-06 02:34:22 +0300

    feat: add multithreading

---
+++ src/main/java/ru/tbank/translator/config/AsyncConfig.java
@@ -0,0 +1,34 @@
+package ru.tbank.translator.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Value("${integration.multithreading.corePoolSize}")
+    private int corePoolSize;
+    @Value("${integration.multithreading.maxPoolSize}")
+    private int maxPoolSize;
+    @Value("${integration.multithreading.queueCapacity}")
+    private int queueCapacity;
+
+    @Override
+    @Bean("asyncExecutor")
+    public ThreadPoolTaskExecutor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(corePoolSize);
+        executor.setMaxPoolSize(maxPoolSize);
+        executor.setQueueCapacity(queueCapacity);
+        executor.setThreadNamePrefix("custom-async-");
+        executor.initialize();
+        return executor;
+    }
+}
--- src/main/java/ru/tbank/translator/service/TranslationService.java
+++ src/main/java/ru/tbank/translator/service/TranslationService.java
@@ -1,32 +1,77 @@
 package ru.tbank.translator.service;
 
 import lombok.RequiredArgsConstructor;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import ru.tbank.translator.mapper.TranslateRequestMapper;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+
+import java.util.concurrent.Future;
+import java.util.concurrent.RejectedExecutionException;
 
 @Service
 @RequiredArgsConstructor
 public class TranslationService {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private final YandexTranslateClientService yandexTranslateClientService;
-    private final TranslateRequestMapper translaterequestMapper;
+    private final TranslateRequestMapper translateRequestMapper;
+    @Qualifier("asyncExecutor")
+    private final ThreadPoolTaskExecutor asyncExecutor;
 
     public void translate(String... args) {
-        // todo: multithreding
+        List<Callable<String>> callables = new ArrayList<>();
         String sourceLanguage = args[0];
         String targetLanguage = args[1];
         List<String> words = new ArrayList<>();
         for (int i = 2; i < args.length; i++) {
-            var requestDTO = translaterequestMapper.argsToRequestDTO(
+            var requestDTO = translateRequestMapper.argsToRequestDTO(
                     sourceLanguage,
                     targetLanguage,
                     args[i]
             );
-            var responseDTO = yandexTranslateClientService.translate(requestDTO);
-            words.add(responseDTO.getTranslations()[0].getText());
+            callables.add(
+                    () -> yandexTranslateClientService
+                            .translate(requestDTO)
+                            .getTranslations()[0]
+                            .getText()
+            );
         }
-        System.out.println(words);
+        List<Future<String>> completableFutureList = new ArrayList<>();
+        for (var callable: callables) {
+            try {
+                completableFutureList.add(asyncExecutor.submit(callable));
+            } catch (RejectedExecutionException e) {
+                String response = "http 500. Ошибка выполнения программы. " + e.getMessage();
+                logger.error(response);
+                return;
+            }
+        }
+
+        for (var completableFuture: completableFutureList) {
+            try {
+                var translatedWord = completableFuture.get();
+                words.add(translatedWord);
+            } catch (HttpClientErrorException e) {
+                String response = "http " + e.getStatusCode() + ". Ошибка выполнения программы. " + e.getMessage();
+                logger.error(response);
+                return;
+            } catch (Exception e) {
+                String response = "http 500. Ошибка выполнения программы. " + e.getMessage();
+                logger.error(response, e);
+                return;
+            }
+        }
+        logger.info("http 200. " + String.join(" ", words));
+
     }
 }
