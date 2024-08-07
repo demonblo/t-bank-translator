3ffa880d7c51e54944f968bb7774e4de3a4cdde4
Dmitry Blokhin <demonblo007@gmail.com>
2024-08-07 08:53:11 +0300

    test: add RejectedExecutionException test

--- src/test/java/ru/tbank/translator/service/TranslationTest.java
+++ src/test/java/ru/tbank/translator/service/TranslationTest.java
@@ -10,6 +10,7 @@ import ru.tbank.translator.repository.TranslationRepository;
 
 import java.net.UnknownHostException;
 import java.util.Collections;
+import java.util.concurrent.RejectedExecutionException;
 
 @SpringBootTest
 public class TranslationTest {
@@ -54,5 +55,31 @@ public class TranslationTest {
 
         Assertions.assertEquals(expTranslation.getInputWords(), translation.getInputWords());
         Assertions.assertEquals(expTranslation.getOutputWords(), translation.getOutputWords());
+    }
+
+    @Test
+    public void tooManyWords() throws UnknownHostException {
+        String sourceLanguage = "en";
+        String targetLanguage = "ru";
+        String sourceWords = "Two households, both alike in dignity,\n" +
+                "In fair Verona, where we lay our scene,\n" +
+                "From ancient grudge break to new mutiny,\n" +
+                "Where civil blood makes civil hands unclean.\n" +
+                "From forth the fatal loins of these two foes\n" +
+                "A pair of star-cross'd lovers take their life;\n" +
+                "Whose misadventured piteous overthrows\n" +
+                "Do with their death bury their parents' strife.\n" +
+                "The fearful passage of their death-mark'd love,\n" +
+                "And the continuance of their parents' rage,\n" +
+                "Which, but their children's end, nought could remove,\n" +
+                "Is now the two hours' traffic of our stage;\n" +
+                "The which if you with patient ears attend,\n" +
+                "What here shall miss, our toil shall strive to mend.";
+
+
+        Assertions.assertThrows(RejectedExecutionException.class, () -> {translationService
+                .translate(sourceLanguage,
+                        targetLanguage,
+                        Collections.singletonList(sourceWords));});
     }
 }
